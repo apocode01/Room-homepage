c640f9f5d415d9b438d6ed5dcc80becdf63c563a
apocode01 <apopanou@gmail.com>
2026-03-16 23:59:28 +0200

    Add mobile menu responsiveness and transitions

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const openMenuBtn = document.querySelector(`.open_menu`);
+const menuPanel = document.querySelector(`.menu_panel`);
+const navbarCategories = document.querySelector(`.navbar_categories`);
+const overlay = document.querySelector(`.overlay`);
 
 openMenuBtn.addEventListener('click', () => {
-    // open menu
+    menuPanel.classList.add(`open`);
+    navbarCategories.classList.remove(`hidden`);
+    overlay.classList.add(`active`);
 })
