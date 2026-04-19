cac55c3bf2ba87b9e8f4106dcce88ac1d98bf1db
apocode01 <apopanou@gmail.com>
2026-04-19 23:46:04 +0300

    finished menu for mobile: added styling and close button functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const openMenuBtn = document.querySelector(`.open_menu`);
 const menuPanel = document.querySelector(`.menu_panel`);
+const closeMenuBtn = document.querySelector(`.close_menu`);
 const navbarCategories = document.querySelector(`.navbar_categories`);
 const overlay = document.querySelector(`.overlay`);
 
@@ -7,4 +8,10 @@ openMenuBtn.addEventListener('click', () => {
     menuPanel.classList.add(`open`);
     navbarCategories.classList.remove(`hidden`);
     overlay.classList.add(`active`);
+})
+
+closeMenuBtn.addEventListener('click', () => {
+    menuPanel.classList.remove(`open`);
+    navbarCategories.classList.add(`hidden`);
+    overlay.classList.remove(`active`);
 })
