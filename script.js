82c9356a89ef6d682341702101f8ce2bdfe0aaea
apocode01 <apopanou@gmail.com>
2026-06-30 19:47:38 +0300

    Enhance hero section with additional images and titles + implement image transition effects

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const menuPanel = document.querySelector(`.menu_panel`);
 const closeMenuBtn = document.querySelector(`.close_menu`);
 const navbarCategories = document.querySelector(`.navbar_categories`);
 const overlay = document.querySelector(`.overlay`);
+const prevBtn = document.querySelector(`.prev_btn`);
+const nextBtn = document.querySelector(`.next_btn`);
+const mainTitle = document.querySelectorAll(`.main_title`)
+const mainDescription = document.querySelectorAll(`.main_description`)
+const heroImg = document.querySelectorAll(`.hero_img`)
+console.log(prevBtn)
 
 openMenuBtn.addEventListener('click', () => {
     menuPanel.classList.add(`open`);
@@ -14,4 +20,30 @@ closeMenuBtn.addEventListener('click', () => {
     menuPanel.classList.remove(`open`);
     navbarCategories.classList.add(`hidden`);
     overlay.classList.remove(`active`);
+})
+
+content_idx = 0
+
+prevBtn.addEventListener('click', () => {
+    mainTitle[content_idx].classList.add(`hidden`);
+    mainDescription[content_idx].classList.add(`hidden`);
+    heroImg[content_idx].classList.add(`hidden`);
+
+    // decrement index and wrap idx 0 to idx 2
+    content_idx = (content_idx + 2) % 3;
+    mainTitle[content_idx].classList.remove(`hidden`);
+    mainDescription[content_idx].classList.remove(`hidden`);
+    heroImg[content_idx].classList.remove(`hidden`);
+})
+
+nextBtn.addEventListener('click', () => {
+    mainTitle[content_idx].classList.add(`hidden`);
+    mainDescription[content_idx].classList.add(`hidden`);
+    heroImg[content_idx].classList.add(`hidden`);
+
+    // increment index and wrap idx 3 to idx 0
+    content_idx = (content_idx + 1) % 3;
+    mainTitle[content_idx].classList.remove(`hidden`);
+    mainDescription[content_idx].classList.remove(`hidden`);
+    heroImg[content_idx].classList.remove(`hidden`);
 })
